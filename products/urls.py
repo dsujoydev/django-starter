5455fd2862fa8769242ac066e8c68eb55530263e
dsujoydev <dsujoy.cse@gmail.com>
2024-09-29 01:20:10 +0600

    feat: post

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from .views import ProductView
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('api/v1/products/', ProductView.as_view(), name="product_op")
 ]
--- products/views.py
+++ products/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.views import APIView
-# from rest_framework.response import Response
+from rest_framework.response import Response 
+from rest_framework import status
+from .serializers import ProductSerializers
+from .models import Product
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at product api")
-
-def ProductView(APIView):
+class ProductView(APIView):
     def post(self, request):
-        pass
-
+        serializer = ProductSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+    
     def get(self, request):
         pass
+        products = Product.objects.all()
+        serializer = ProductSerializers(products, many=True)
+        return Response(serializer.data)
